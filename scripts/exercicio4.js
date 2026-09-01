35d188540bc963e7c9d0fef4a24898bc13b12157
vr-ouroboros <vitor.rodriiguees@hotmail.com>
2026-09-01 16:53:01 -0300

    feat: coleta e apresenta dados dos alunos

---
+++ scripts/exercicio4.js
@@ -0,0 +1,9 @@
+const idade = prompt("Digite sua idade:");
+
+console.log("Sua idade é: " + idade);
+console.log(typeof idade);
+
+Number(idade);
+let idadeFutura = idade + 5;
+
+console.log("Sua idade em 5 anos será: " + idadeFutura);
