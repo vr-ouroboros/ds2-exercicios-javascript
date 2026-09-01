b1d923e4bad2dae21a1df6e98f70a4cbc0affbca
vr-ouroboros <vitor.rodriiguees@hotmail.com>
2026-09-01 16:02:08 -0300

    feat: adiciona primeiro script e evidência do console

---
+++ scripts/exercicio1.js
@@ -0,0 +1,9 @@
+const nomeAluno = "Vitor Soares Rodrigues";
+const nomeCurso = "Sistemas para Internet";
+const func = "Desenvolver uma API";
+
+alert("JavaScript carregado com sucesso!");
+
+console.log(nomeAluno);
+console.log(nomeCurso);
+console.log(func);
